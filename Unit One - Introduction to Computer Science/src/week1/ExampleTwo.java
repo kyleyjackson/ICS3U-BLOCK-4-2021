7e049839d36b3a3a722aa0aacf2d388656e1396d
Kyle J <kjackson@bayviewglen.ca>
2021-09-09 14:49:11 -0400

    Unit One 1st Commit

---
+++ Unit One - Introduction to Computer Science/src/week1/ExampleTwo.java
@@ -0,0 +1,16 @@
+package week1;
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+        System.out.println("A");
+        System.out.print("B");
+        System.out.print("C");
+        System.out.println("D");
+        System.out.println("E");
+        // String literal -> "Hello"
+        // Not a String literal -> 'Hello'
+        /*
+         * Other random type of comment
+         */
+    }
+}
