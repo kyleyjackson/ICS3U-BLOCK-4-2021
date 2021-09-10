e9b3b2c8e73443e666a173b0989144429d6f8aa0
Kyle J <kjackson@bayviewglen.ca>
2021-09-10 10:54:18 -0400

    First Homework Commit

---
+++ Unit One - Introduction to Computer Science/src/week1/Quadratic.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class Quadratic {
+    public static void main(String[] args) {
+        double a = 10;
+        double b = 20;
+        double c = 30;
+        double x = 40;
+        double equation = ((a * x) * (a * x) + b * x + c);
+        System.out.println(equation);
+    }
+
+}
