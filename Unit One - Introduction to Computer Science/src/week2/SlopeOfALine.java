9dabcff7b5a6f54f14f32d1fad071c7e1c9d196f
Kyle J <kjackson@bayviewglen.ca>
2021-09-14 13:58:56 -0400

    Homework - Week 2

---
+++ Unit One - Introduction to Computer Science/src/week2/SlopeOfALine.java
@@ -0,0 +1,12 @@
+package week1;
+
+public class SlopeOfALine {
+    public static void main(String[] args) {
+        double x1 = 10;
+        double x2 = 15;
+        double y1 = 25;
+        double y2 = 20;
+        double slope = (y2 - y1) / (x2 - x1);
+        System.out.print(slope);
+    }
+}
