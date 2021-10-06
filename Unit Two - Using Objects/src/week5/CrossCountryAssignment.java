84b03b2d1a37ddbe3eb7e370dd93d2b910c211b9
Kyle J <kjackson@bayviewglen.ca>
2021-10-06 08:32:17 -0400

    Cross Country Assignment

---
+++ Unit Two - Using Objects/src/week5/CrossCountryAssignment.java
@@ -0,0 +1,89 @@
+package week5;
+
+import java.util.Scanner;
+
+public class CrossCountryAssignment {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        processRunner(in);
+        processRunner(in);
+        processRunner(in);
+        in.close();
+    }
+
+    private static void processRunner(Scanner in) {
+        String firstName, lastName;
+        String mileOne, mileTwo, finish;
+        String splitTwo, splitThree;
+        /**
+         * Get first & last name of runner Get the mileOne, mileTwo, and finish times
+         * for the runner
+         */
+        System.out.print("Please enter your first name: ");
+        firstName = in.nextLine();
+
+        System.out.print("Please enter your last name: ");
+        lastName = in.nextLine();
+
+        System.out.print("Please enter your 1 mile time: ");
+        mileOne = in.nextLine();
+
+        System.out.print("Please enter your 2 mile time: ");
+        mileTwo = in.nextLine();
+
+        System.out.print("Please enter your finishing time: ");
+        finish = in.nextLine();
+
+        splitTwo = subtractTimes(mileTwo, mileOne);
+        splitThree = subtractTimes(finish, mileTwo);
+
+        // Prints out all of the runner's information
+        System.out.println("Runner " + firstName + " " + lastName + "'s times are as follows.");
+        System.out.println("First split: " + mileOne);
+        System.out.println("Second split: " + splitTwo);
+        System.out.println("Third split: " + splitThree);
+        System.out.println("Finishing time: " + finish);
+        System.out.println("\n--------------------------------------------------------------------------\n ");
+    }
+
+    private static String subtractTimes(String endTime, String startTime) {
+        double endTimeInSeconds = convertToSeconds(endTime);
+        double startTimeInSeconds = convertToSeconds(startTime);
+
+        double diffInSeconds = endTimeInSeconds - startTimeInSeconds;
+
+        return convertToTime(diffInSeconds);
+    }
+
+    private static String convertToTime(double diffInSeconds) {
+        // Converts from seconds, into the standard format 0:00.000
+        int minutes = (int) diffInSeconds / 60;
+        double seconds = diffInSeconds % 60;
+        if (seconds < 10) {
+            return minutes + ":" + "0" + String.format("%.3f", seconds);
+        } else {
+            return minutes + ":" + String.format("%.3f", seconds);
+        }
+
+    }
+
+    private static double convertToSeconds(String endTime) {
+        double seconds;
+        int seconds1;
+        // Checks index of the colon in the time
+        if (endTime.indexOf(":") == 1) {
+            // Parses the strings excluding the double, based on the index of the colon
+            seconds = Double.parseDouble(endTime.substring(2));
+            seconds1 = Integer.parseInt(endTime.substring(0, 1));
+        } else if (endTime.indexOf(":") == 2) {
+            seconds = Double.parseDouble(endTime.substring(3));
+            seconds1 = Integer.parseInt(endTime.substring(0, 2));
+        } else {
+            seconds = Double.parseDouble(endTime.substring(4));
+            seconds1 = Integer.parseInt(endTime.substring(0, 3));
+        }
+        // Returns the combined value of both variables
+        return (seconds1 * 60) + seconds;
+    }
+}
